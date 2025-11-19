fb3afe0a32b2bc6d24dd3cd9a86a2283906dc814
kanishk mishra <kanishkm794@gmail.com>
2025-11-19 15:42:06 +0530

    Basic frontend backend setup and defiend mvp architecture

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>hello world</h1>
+    </>
+  )
+}
+
+export default App
