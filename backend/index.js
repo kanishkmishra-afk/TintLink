9e1e4c7bafc78b6d79be21631e934a7f4cafc6c2
kanishk mishra <kanishkm794@gmail.com>
2025-11-20 18:49:29 +0530

    chore: update CORS_ORIGIN to use production URL

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:5173'
+const CORS_ORIGIN ='https://tintlink-frontend.onrender.com'
 
 app.use(express.json())
 app.use(cors({ origin: CORS_ORIGIN }))
